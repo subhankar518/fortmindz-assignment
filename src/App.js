08205aafac88b83d25b5be205872457105b3cd97
Subhankar Manna <subhankarmanna518@gmail.com>
2024-10-20 19:53:16 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,3 @@
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { EmployeeList } from "./components/EmployeeList";
-
-// function App() {
-//   return (
-//     <div>
-//       <EmployeeList/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { Suspense, lazy } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
